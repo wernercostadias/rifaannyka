533da9313e32c5f9320f117d5e0f7b6eb85ea213
Werner Costa Dias <wernerdiasdias123@gmail.com>
2026-06-06 05:14:45 -0300

    feat: atualizar configuração do banco de dados para usar env.db_url_config

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -4,5 +4,5 @@ from .base import *  # noqa: F403
 DEBUG = True
 
 DATABASES = {
-    "default": env.db(get_database_url(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"))  # noqa: F405
+    "default": env.db_url_config(get_database_url(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"))  # noqa: F405
 }
